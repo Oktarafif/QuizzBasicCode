f0c04e701ca4f0330f3732237f61f5d78a0a188c
Oktarafif <rafifokt@gmail.com>
2020-12-28 13:35:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quizz2/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.quizz2;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    AlertDialog.Builder builder;
+    RadioGroup radioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+    }
+    public void onRadioButton(View view){
+
+        Boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()){
+            case R.id.radioButton:
+                if (checked)
+                    tampilDialog();
+                break;
+
+            case R.id.radioButton2:
+                if (checked)
+                    jawabanSalah();
+                break;
+            case R.id.radioButton3:
+                if (checked)
+                    jawabanSalah();
+                break;
+            case R.id.radioButton4:
+                if (checked)
+                    jawabanSalah();
+                break;
+        }
+    }
+
+    public void tampilDialog(){
+
+        builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setTitle("Selamat !!!");
+        builder.setMessage("Jawaban Kamu Benar : Real Madrid");
+        builder.setPositiveButton("OKE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Selamat", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("ULANGI", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                radioGroup.clearCheck();
+            }
+        });
+        builder.create().show();
+    }
+
+    public void jawabanSalah(){
+        Toast.makeText(this, "Jawaban Kamu Salah", Toast.LENGTH_SHORT).show();
+    }
+}
